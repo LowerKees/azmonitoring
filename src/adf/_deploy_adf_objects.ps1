7757efca844b0cbaa0d8b79c5aad67aeda743a92
Martijn Beenker <martijn.beenker@avanade.com>
2021-01-03 10:31:27 +0100

    Add production deployment for adf

--- src/adf/_deploy_adf_objects.ps1
+++ src/adf/_deploy_adf_objects.ps1
@@ -1,72 +1,75 @@
 $RESOURCEGROUP = "rg-atana-001"
-$DATAFACTORYNAME = "adf-at-ana-dev"
+$DATAFACTORYNAMES = @("adf-at-ana-dev", "adf-at-ana-prod")
 
-# Resources that are valid
-New-AzDataFactoryV2LinkedService `
-    -Name "LS_ADLS2_AT" `
-    -ResourceGroupName $RESOURCEGROUP `
-    -DataFactoryName $DATAFACTORYNAME `
-    -DefinitionFile $PSScriptRoot\ls_adls2_at.json `
-    -Force `
-    -ErrorAction Stop
+foreach ($DataFactoryName in $DATAFACTORYNAMES) {
 
-New-AzDataFactoryV2Dataset `
-    -Name "DS_ADLS2_BIN" `
-    -ResourceGroupName $RESOURCEGROUP `
-    -DataFactoryName $DATAFACTORYNAME `
-    -DefinitionFile $PSScriptRoot\ds_adls2_bin.json `
-    -Force `
-    -ErrorAction Stop
+    # Resources that are valid
+    New-AzDataFactoryV2LinkedService `
+        -Name "LS_ADLS2_AT" `
+        -ResourceGroupName $RESOURCEGROUP `
+        -DataFactoryName $DAtaFactoryName `
+        -DefinitionFile $PSScriptRoot\ls_adls2_at.json `
+        -Force `
+        -ErrorAction Stop
 
-New-AzDataFactoryV2Pipeline `
-    -Name "PL_MOVE_DATA" `
-    -ResourceGroupName $RESOURCEGROUP `
-    -DataFactoryName $DATAFACTORYNAME `
-    -DefinitionFile $PSScriptRoot\pl_move_data.json `
-    -Force `
-    -ErrorAction Stop
+    New-AzDataFactoryV2Dataset `
+        -Name "DS_ADLS2_BIN" `
+        -ResourceGroupName $RESOURCEGROUP `
+        -DataFactoryName $DAtaFactoryName `
+        -DefinitionFile $PSScriptRoot\ds_adls2_bin.json `
+        -Force `
+        -ErrorAction Stop
 
-# Resources that will display a runtime error because of a non existing storage container
-New-AzDataFactoryV2LinkedService `
-    -Name "LS_ADLS2_AT_B" `
-    -ResourceGroupName $RESOURCEGROUP `
-    -DataFactoryName $DATAFACTORYNAME `
-    -DefinitionFile $PSScriptRoot\ls_adls2_at_b.json `
-    -Force `
-    -ErrorAction Stop
+    New-AzDataFactoryV2Pipeline `
+        -Name "PL_MOVE_DATA" `
+        -ResourceGroupName $RESOURCEGROUP `
+        -DataFactoryName $DAtaFactoryName `
+        -DefinitionFile $PSScriptRoot\pl_move_data.json `
+        -Force `
+        -ErrorAction Stop
 
-New-AzDataFactoryV2Dataset `
-    -Name "DS_ADLS2_BIN_B" `
-    -ResourceGroupName $RESOURCEGROUP `
-    -DataFactoryName $DATAFACTORYNAME `
-    -DefinitionFile $PSScriptRoot\ds_adls2_bin_b.json `
-    -Force `
-    -ErrorAction Stop
+    # Resources that will display a runtime error because of a non existing storage container
+    New-AzDataFactoryV2LinkedService `
+        -Name "LS_ADLS2_AT_B" `
+        -ResourceGroupName $RESOURCEGROUP `
+        -DataFactoryName $DAtaFactoryName `
+        -DefinitionFile $PSScriptRoot\ls_adls2_at_b.json `
+        -Force `
+        -ErrorAction Stop
 
-New-AzDataFactoryV2Pipeline `
-    -Name "PL_MOVE_DATA_B" `
-    -ResourceGroupName $RESOURCEGROUP `
-    -DataFactoryName $DATAFACTORYNAME `
-    -DefinitionFile $PSScriptRoot\pl_move_data_b.json `
-    -Force `
-    -ErrorAction Stop
+    New-AzDataFactoryV2Dataset `
+        -Name "DS_ADLS2_BIN_B" `
+        -ResourceGroupName $RESOURCEGROUP `
+        -DataFactoryName $DAtaFactoryName `
+        -DefinitionFile $PSScriptRoot\ds_adls2_bin_b.json `
+        -Force `
+        -ErrorAction Stop
 
-$Trigger = Get-AzDataFactoryV2Trigger `
-    -Name "TR_DAILY" `
-    -ResourceGroupName $RESOURCEGROUP `
-    -DataFactoryName $DATAFACTORYNAME `
-    -ErrorAction SilentlyContinue
+    New-AzDataFactoryV2Pipeline `
+        -Name "PL_MOVE_DATA_B" `
+        -ResourceGroupName $RESOURCEGROUP `
+        -DataFactoryName $DAtaFactoryName `
+        -DefinitionFile $PSScriptRoot\pl_move_data_b.json `
+        -Force `
+        -ErrorAction Stop
 
-if ($Trigger) {
-    Stop-AzDataFactoryV2Trigger -InputObject $Trigger
+    $Trigger = Get-AzDataFactoryV2Trigger `
+        -Name "TR_DAILY" `
+        -ResourceGroupName $RESOURCEGROUP `
+        -DataFactoryName $DAtaFactoryName `
+        -ErrorAction SilentlyContinue
+
+    if ($Trigger) {
+        Stop-AzDataFactoryV2Trigger -InputObject $Trigger
+    }
+
+    $Trigger = New-AzDataFactoryV2Trigger `
+        -Name "TR_DAILY" `
+        -ResourceGroupName $RESOURCEGROUP `
+        -DataFactoryName $DAtaFactoryName `
+        -DefinitionFile $PSScriptRoot\tr_daily.json `
+        -Force `
+        -ErrorAction Stop
+
+    Start-AzDataFactoryV2Trigger -InputObject $Trigger
 }
-
-$Trigger = New-AzDataFactoryV2Trigger `
-    -Name "TR_DAILY" `
-    -ResourceGroupName $RESOURCEGROUP `
-    -DataFactoryName $DATAFACTORYNAME `
-    -DefinitionFile $PSScriptRoot\tr_daily.json `
-    -Force `
-    -ErrorAction Stop
-
-Start-AzDataFactoryV2Trigger -InputObject $Trigger
